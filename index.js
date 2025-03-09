69356959da6dde2b9b790ea8fb2e453bcaa369fa
Emman Oduor <wyllademanu003@gmail.com>
2025-03-09 16:49:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  const drivers = ["Sam", "Sarah", "John", "Samantha"];
+
+console.log(findMatching(drivers, "sam")); 
+
+console.log(fuzzyMatch(drivers, "Sa")); 
+
+const driverObjects = [
+  { name: "Sam", hometown: "New York" },
+  { name: "Sarah", hometown: "Chicago" },
+  { name: "John", hometown: "Los Angeles" }
+];
+
+console.log(matchName(driverObjects, "Sam"));
